2b2c2158552133695901accd8468b3c6ab6d3b4c
Akshaya Biswal <mraxya@gmail.com>
2023-12-21 18:44:27 +0530

    Handle errors using trycatch

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,16 +1,41 @@
-import { Response } from "express";
+/*
+- Here, we have exported the class "AuthController",
+- when ever we want to use it, we have to create a instance and after that we can use it. we are doing this to do "Dependency Injection"
+*/
+
+import { Logger } from "winston";
+import { NextFunction, Response } from "express";
+
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
 
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private logger: Logger,
+  ) {}
 
-  async register(req: RegisterUserRequest, res: Response) {
+  async register(req: RegisterUserRequest, res: Response, next: NextFunction) {
     const { firstName, lastName, email, password } = req.body;
-    await this.userService.create({ firstName, lastName, email, password });
-    res.status(201).json();
+
+    this.logger.debug("New request to register a user", {
+      firstName,
+      lastName,
+      email,
+    });
+
+    try {
+      const user = await this.userService.create({
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+      this.logger.info("User has been registered", { id: user.id });
+      res.status(201).json({ id: user.id });
+    } catch (error) {
+      next(error);
+      return;
+    }
   }
 }
-
-// Here, we have exported the class "AuthController",
-// when ever we want to use it, we have to create a instance and after that we can use it. we are doing this to do "Dependency Injection"
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,13 +4,16 @@ import { User } from "../entity/User";
 import { UserService } from "../services/UserService";
 import { AppDataSource } from "../config/data-source";
 import { AuthController } from "../controller/AuthController";
+import { logger } from "../config/logger";
 
 const authRouter = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const authController = new AuthController(userService);
+const authController = new AuthController(userService, logger);
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-authRouter.post("/register", (req, res) => authController.register(req, res));
+authRouter.post("/register", (req, res, next) =>
+  authController.register(req, res, next),
+);
 
 export default authRouter;
