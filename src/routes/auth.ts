5f6ed11a7e1de1dba490c13b60bcfc35045387e7
Akshaya Biswal <mraxya@gmail.com>
2023-12-26 17:46:01 +0530

    Refactoring token service

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,13 +4,15 @@ import { User } from "../entity/User";
 import { logger } from "../config/logger";
 import { UserService } from "../services/UserService";
 import { AppDataSource } from "../config/data-source";
+import { TokenService } from "../services/TokenService";
 import { AuthController } from "../controller/AuthController";
 import registerValidator from "../validators/register-validator";
 
 const authRouter = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const authController = new AuthController(userService, logger);
+const tokenService = new TokenService();
+const authController = new AuthController(userService, logger, tokenService);
 
 authRouter.post(
   "/register",
