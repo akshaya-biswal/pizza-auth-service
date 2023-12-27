c1f0aaebd62f14750a569ae9778dd9f29b9f6cd6
Akshaya Biswal <mraxya@gmail.com>
2023-12-27 11:38:16 +0530

    Add DB connection to sever

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import app from "./app";
 import { Config } from "./config";
+import { AppDataSource } from "./config/data-source";
 import { logger } from "./config/logger";
 
-const startServer = () => {
+const startServer = async () => {
   const PORT = Config.PORT;
   try {
-    logger.debug("debug message", {});
+    await AppDataSource.initialize();
+    logger.info("Database connected successfully");
     app.listen(PORT, () => logger.info(`Listening on PORT ${PORT}`));
   } catch (err: unknown) {
     if (err instanceof Error) {
@@ -17,4 +19,4 @@ const startServer = () => {
   }
 };
 
-startServer();
+void startServer();
